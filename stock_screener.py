f0b4fb90d709f00a35f0da2abe05022d40099ace
Andrew Pianta <a.pianta92@gmail.com>
2020-08-29 20:14:46 -0400

    Update stock_screener.py algorithm

--- stock_screener.py
+++ stock_screener.py
@@ -11,10 +11,103 @@ yf.pdr_override()
 
 stocklist = si.tickers_sp500()
 index_name = '^GSPC'  # S&P 500
-
 final = []
 index = []
 n = -1
 
 exportList = pd.DataFrame(columns=['Stock', "RS_Rating", "50 Day MA",
                                    "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High"])
+
+for stock in stocklist:
+    n += 1
+    time.sleep(1)  # hold the code back 1 second
+
+    print('\npulling {} with index {}'.format(stock, n))
+
+    # RS_Rating
+    start_date = datetime.datetime.now() - datetime.timedelta(days=365)
+    end_date = datetime.date.today()
+
+    df = pdr.get_data_yahoo(stock, start=start_date, end=end_date)
+    df['Percent Change'] = df['Adj Close'].pct_change()
+    stock_return = df['Percent Change'].sum() * 100
+    index_df = pdr.get_data_yahoo(index_name, start=start_date, end=end_date)
+    index_df['Perfect Change'] = index_df['Adj Close'].pct_Change()
+    index_return = index_df['Percent Change'].sum() * 100
+
+# rs_rating is calculated by dividing the % change of the stock from the past year to the % change of the overall market... It essentiall shows whther the stock is performing at, above, or below the standard.
+
+    RS_Rating = round((stock_return / index_return) * 10, 2)
+    try:
+        sma = [50, 150, 200]
+        for x in sma:
+            df['SMA+'+str(x)] = round(df.iloc[:,
+                                              4].rolling(window=x).mean(), 2)
+        currentClose = df['Adj Close'][-1]
+        moving_average_50 = df['SMA_50'][-1]
+        moving_average_150 = df['SMA_150'][-1]
+        moving_average_200 = df['SMA_200'][-1]
+        low_of_52week = min(df['Adj Close'][-260:])
+        high_of_52week = max(df['Adj Close'][-260:])
+
+        try:
+            moving_average_200_20 = df['SMA_200'][-20]
+
+        except Exception:
+            moving_average_200_20 = 0
+
+        # condition 1: current price > 150 SMA and > 200 SMA
+        if(currentClose > moving_average_150 > moving_average_200):
+            condition_1 = True
+
+        else:
+            condition_1 = False
+            # condition 2: 150 sma and > 200 sma
+        if(moving_average_150 > moving_average_200):
+            condition_2 = True
+        else:
+            condition_2 = False
+
+        # condition 3: 200 sma trending up for at least 1 month (ideally 4-5 months)
+        if(moving_average_200 > moving_average_200_20):
+            condition_3 = True
+        else:
+            condition_3 = False
+        if(moving_average_50 > moving_average_150 > moving_average_200):
+            condition_4 = True
+        else:
+            condition_4 = False
+        if(currentClose > moving_average_50):
+            condition_5 = True
+        else:
+            condition_5 = False
+        if(currentClose >= (1.3 * low_of_52week)):
+            condition_6 = True
+        else:
+            condition_6 = False
+        if(currentClose >= (.75*high_of_52week)):
+            condition_7 = True
+        else:
+            condition_7 = False
+        if(RS_Rating >= 70):
+            condition_8 = True
+        else:
+            condition_8 = False
+
+        if(condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8):
+            final.append(stock)
+            index.append(n)
+            dataframe = pd.DataFrame(
+                list(zip(final, index)), columns=['Company', 'Index'])
+            dataframe.to_csv('stocks.csv')
+
+            exportList = exportList.append({'Stock': stock, 'RS_Rating': '50 Day MA': moving_average_50, '150 Day Ma': moving_average_150, '200 Day MA': moving_average_200, '52 Week Low': low_of_52week, '52 week High': high_of_52week}, ignore_index=True)
+            print(stock + ' made the requirements')
+    except Exception as e:
+        print(e)
+        print('No data on "+ stock)
+print(exportList)
+
+writer = ExcelWriter('ScreenOutput.xlsxs')
+exportList.to_excel(writer, 'Sheet1')
+writer.save()
